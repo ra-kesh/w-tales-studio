41d72c0acddaf2f092b17fef85a7977d222414f4
ra-kesh <iamrakeshkumar@pm.me>
2025-03-22 21:28:02 +0530

    added initial table for tasks

--- src/app/(dashboard)/tasks/page.tsx
+++ src/app/(dashboard)/tasks/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
 
-const Tasks = () => {
-  return <div className="flex flex-1 flex-col gap-4 p-4 pt-0">tasks</div>;
+import { DataTable } from "./components/data-table";
+import { taskSchema } from "./data/schema";
+import { columns } from "./components/columns";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
 };
 
-export default Tasks;
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "src/app/(dashboard)/tasks/data/tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
