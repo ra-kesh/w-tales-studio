60e71103c9226c9c1c7038fe8aa56ccbb5855e3c
ra-kesh <iamrakeshkumar@pm.me>
2025-03-26 18:07:50 +0530

    changed dashboard routed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,33 +1,33 @@
-import * as React from "react";
+import type * as React from "react";
 import { AppSidebar } from "@/components/app-sidebar";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
 } from "@/components/ui/sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            {/* <Breadcrumb>
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+						{/* <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">Building Your </BreadcrumbLink>
@@ -38,12 +38,12 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb> */}
-          </div>
-        </header>
-        <div>{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+					</div>
+				</header>
+				<div>{children}</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 };
 
 export default DashboardLayout;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const wtp = () => {
+	return <div>wtp</div>;
+};
+
+export default wtp;
