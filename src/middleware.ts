ddf770aa0520ea06a1cfda0cc8f778323ee5d269
ra-kesh <iamrakeshkumar@pm.me>
2025-03-30 13:42:57 +0530

    fix middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,31 +5,31 @@ import { NextResponse } from "next/server";
 const client = createAuthClient();
 
 export async function middleware(request: NextRequest) {
-  const { data: session } = await client.getSession({
-    fetchOptions: {
-      headers: {
-        cookie: request.headers.get("cookie") || "",
-      },
-    },
-  });
+	const { data: session } = await client.getSession({
+		fetchOptions: {
+			headers: {
+				cookie: request.headers.get("cookie") || "",
+			},
+		},
+	});
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
-  return NextResponse.next();
+	if (!session) {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/bookings/:path*",
-    "/dashboard/:path*",
-    "/clients/:path*",
-    "/deliverables/:path*",
-    "/expenses/:path*",
-    "/shoots/:path*",
-    "/tasks/:path*",
-    "/teams/:path*",
-    "/configurations/:path*",
-    "settings/:path*",
-  ],
+	matcher: [
+		"/bookings/:path*",
+		"/dashboard/:path*",
+		"/clients/:path*",
+		"/deliverables/:path*",
+		"/expenses/:path*",
+		"/shoots/:path*",
+		"/tasks/:path*",
+		"/teams/:path*",
+		"/configurations/:path*",
+		"/settings/:path*",
+	],
 };
