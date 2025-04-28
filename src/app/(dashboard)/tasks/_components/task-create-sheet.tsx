cc6a2164986d7776d6d1a078e8e9ef5a0118dafa
ra-kesh <iamrakeshkumar@pm.me>
2025-04-28 12:31:17 +0530

    added leftover mutateasync

--- src/app/(dashboard)/tasks/_components/task-create-sheet.tsx
+++ src/app/(dashboard)/tasks/_components/task-create-sheet.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
+	Sheet,
+	SheetContent,
+	SheetHeader,
+	SheetTitle,
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
@@ -15,37 +15,37 @@ import { useTaskParams } from "@/hooks/use-task-params";
 import { useCreateTaskMutation } from "@/hooks/use-task-mutation";
 
 export function TaskCreateSheet() {
-  const { setParams, createTask } = useTaskParams();
-  const isOpen = Boolean(createTask);
+	const { setParams, createTask } = useTaskParams();
+	const isOpen = Boolean(createTask);
 
-  const createTaskMutation = useCreateTaskMutation();
+	const createTaskMutation = useCreateTaskMutation();
 
-  const handleSubmit = async (data: TaskFormValues) => {
-    try {
-      await createTaskMutation.mutate(data);
-      setParams(null);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const handleSubmit = async (data: TaskFormValues) => {
+		try {
+			await createTaskMutation.mutateAsync(data);
+			setParams(null);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  return (
-    <Sheet open={isOpen} onOpenChange={() => setParams(null)}>
-      <SheetContent side="right" className="min-w-xl">
-        <SheetHeader className="flex justify-between items-center flex-row">
-          <SheetTitle className="text-xl">Create Task</SheetTitle>
-          <Button
-            size="icon"
-            variant="ghost"
-            onClick={() => setParams(null)}
-            className="p-0 m-0 size-auto hover:bg-transparent"
-          >
-            <X className="size-4" />
-          </Button>
-        </SheetHeader>
+	return (
+		<Sheet open={isOpen} onOpenChange={() => setParams(null)}>
+			<SheetContent side="right" className="min-w-xl">
+				<SheetHeader className="flex justify-between items-center flex-row">
+					<SheetTitle className="text-xl">Create Task</SheetTitle>
+					<Button
+						size="icon"
+						variant="ghost"
+						onClick={() => setParams(null)}
+						className="p-0 m-0 size-auto hover:bg-transparent"
+					>
+						<X className="size-4" />
+					</Button>
+				</SheetHeader>
 
-        <TaskForm onSubmit={handleSubmit} mode="create" />
-      </SheetContent>
-    </Sheet>
-  );
+				<TaskForm onSubmit={handleSubmit} mode="create" />
+			</SheetContent>
+		</Sheet>
+	);
 }
