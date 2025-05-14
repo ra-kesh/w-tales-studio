9902e55c399e589df76069a361ac15b0bbf4947c
ra-kesh <iamrakeshkumar@pm.me>
2025-05-14 17:32:31 +0530

    fix styling

--- src/app/(dashboard)/bookings/(bookingwithdetail)/[id]/_components/booking-list-pagination.tsx
+++ src/app/(dashboard)/bookings/(bookingwithdetail)/[id]/_components/booking-list-pagination.tsx
@@ -9,13 +9,6 @@ import {
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
 
 interface DataTablePaginationProps<TData> {
 	table: Table<TData>;
@@ -25,12 +18,8 @@ export function BookingListPagination<TData>({
 	table,
 }: DataTablePaginationProps<TData>) {
 	return (
-		<div className="flex items-center justify-between p-2">
-			<div className="flex-1 text-sm text-muted-foreground">
-				{table.getFilteredSelectedRowModel().rows.length} of{" "}
-				{table.getFilteredRowModel().rows.length} row(s) selected.
-			</div>
-			<div className="flex items-center space-x-6 lg:space-x-8">
+		<div className="flex  items-center  py-2">
+			<div className="flex min-w-full items-center justify-between space-x-6 ">
 				<div className="flex w-[100px] items-center justify-center text-sm font-medium">
 					Page {table.getState().pagination.pageIndex + 1} of{" "}
 					{table.getPageCount()}
