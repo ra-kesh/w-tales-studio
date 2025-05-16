1a0b9fcc51c92dfb8f6ff5f5a207f67dbdb4f510
ra-kesh <iamrakeshkumar@pm.me>
2025-05-16 10:00:23 +0530

    added nav user in site header

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-	BadgeCheck,
-	Bell,
-	ChevronsUpDown,
-	CreditCard,
-	LogOut,
-	Sparkles,
-} from "lucide-react";
+import { LogOut } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -17,21 +10,16 @@ import {
 	DropdownMenuItem,
 	DropdownMenuLabel,
 	DropdownMenuSeparator,
+	DropdownMenuShortcut,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	useSidebar,
-} from "@/components/ui/sidebar";
+
 import { signOut } from "@/lib/auth/auth-client";
 import { useRouter } from "next/navigation";
 import type { User } from "better-auth";
+import { Button } from "./ui/button";
 
 export function NavUser({ user }: { user: User }) {
-	const { isMobile } = useSidebar();
-
 	const router = useRouter();
 
 	async function handleSignOut() {
@@ -45,77 +33,50 @@ export function NavUser({ user }: { user: User }) {
 	}
 
 	return (
-		<SidebarMenu>
-			<SidebarMenuItem>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<SidebarMenuButton
-							size="lg"
-							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-						>
-							<Avatar className="h-8 w-8 rounded-lg">
-								<AvatarImage src={user?.image as string} alt={user?.name} />
-								<AvatarFallback className="rounded-lg">CN</AvatarFallback>
-							</Avatar>
-							<div className="grid flex-1 text-left text-sm leading-tight">
-								<span className="truncate font-medium">{user?.name}</span>
-								<span className="truncate text-xs">{user?.email}</span>
-							</div>
-							<ChevronsUpDown className="ml-auto size-4" />
-						</SidebarMenuButton>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent
-						className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-						side={isMobile ? "bottom" : "right"}
-						align="end"
-						sideOffset={4}
-					>
-						<DropdownMenuLabel className="p-0 font-normal">
-							<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-								<Avatar className="h-8 w-8 rounded-lg">
-									<AvatarImage src={user?.image as string} alt={user?.name} />
-									<AvatarFallback className="rounded-lg">CN</AvatarFallback>
-								</Avatar>
-								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-medium">{user?.name}</span>
-									<span className="truncate text-xs">{user?.email}</span>
-								</div>
-							</div>
-						</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem>
-								<Sparkles />
-								Upgrade to Pro
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem>
-								<BadgeCheck />
-								Account
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<CreditCard />
-								Billing
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<Bell />
-								Notifications
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<form action={handleSignOut}>
-							<button type="submit" className="w-full">
-								<DropdownMenuItem>
-									<LogOut />
-									Log out
-								</DropdownMenuItem>
-							</button>
-						</form>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</SidebarMenuItem>
-		</SidebarMenu>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost" className="relative h-8 w-8 rounded-full">
+					<Avatar className="h-8 w-8">
+						<AvatarImage src={user?.image as string} alt={user?.name} />
+						<AvatarFallback className="rounded-lg">td</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56" align="end" forceMount>
+				<DropdownMenuLabel className="font-normal">
+					<div className="flex flex-col space-y-1">
+						<p className="text-sm font-medium leading-none">{user?.name}</p>
+						<p className="text-xs leading-none text-muted-foreground">
+							{user?.email}
+						</p>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						Profile
+						<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						Billing
+						<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						Settings
+						<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<form action={handleSignOut}>
+					<button type="submit" className="w-full">
+						<DropdownMenuItem>
+							<LogOut />
+							Log out
+							<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+						</DropdownMenuItem>
+					</button>
+				</form>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 }
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,49 +3,57 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { generateBreadcrumbs } from "@/lib/utils";
+import { useSession } from "@/lib/auth/auth-client";
+import { NavUser } from "./nav-user";
+import type { User } from "@/lib/db/schema";
 
 export function SiteHeader() {
-  const pathname = usePathname();
-  const breadcrumbs = useMemo(() => generateBreadcrumbs(pathname), [pathname]);
-  return (
-    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-          <Breadcrumb>
-            <BreadcrumbList>
-              {breadcrumbs.map((breadcrumb, index) => (
-                <BreadcrumbItem key={breadcrumb.href}>
-                  {breadcrumb.isLast ? (
-                    <BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
-                  ) : (
-                    <>
-                      <BreadcrumbLink href={breadcrumb.href}>
-                        {breadcrumb.label}
-                      </BreadcrumbLink>
-                      <BreadcrumbSeparator />
-                    </>
-                  )}
-                </BreadcrumbItem>
-              ))}
-            </BreadcrumbList>
-          </Breadcrumb>
-        </div>
-      </div>
-    </header>
-  );
+	const pathname = usePathname();
+	const breadcrumbs = useMemo(() => generateBreadcrumbs(pathname), [pathname]);
+
+	const { data } = useSession();
+	return (
+		<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-b">
+			<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6 justify-between">
+				<div className="flex items-center gap-2 px-4">
+					<SidebarTrigger className="-ml-1" />
+					<Separator
+						orientation="vertical"
+						className="mr-2 data-[orientation=vertical]:h-4"
+					/>
+					<Breadcrumb>
+						<BreadcrumbList>
+							{breadcrumbs.map((breadcrumb, index) => (
+								<BreadcrumbItem key={breadcrumb.href}>
+									{breadcrumb.isLast ? (
+										<BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
+									) : (
+										<>
+											<BreadcrumbLink href={breadcrumb.href}>
+												{breadcrumb.label}
+											</BreadcrumbLink>
+											<BreadcrumbSeparator />
+										</>
+									)}
+								</BreadcrumbItem>
+							))}
+						</BreadcrumbList>
+					</Breadcrumb>
+				</div>
+				<div>
+					<NavUser user={data?.user as User} />
+				</div>
+			</div>
+		</header>
+	);
 }
