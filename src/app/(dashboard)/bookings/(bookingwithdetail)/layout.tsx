08a1fcfe85f1b0531736298fd8ec8e073b46db77
ra-kesh <iamrakeshkumar@pm.me>
2025-05-25 17:16:32 +0530

    changed booking stats data fetching

--- src/app/(dashboard)/bookings/(bookingwithdetail)/layout.tsx
+++ src/app/(dashboard)/bookings/(bookingwithdetail)/layout.tsx
@@ -1,43 +1,41 @@
 import { getServerSession } from "@/lib/dal";
 import { BookingStats } from "./_components/booking-stats";
 import {
-	dehydrate,
-	HydrationBoundary,
-	QueryClient,
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
 } from "@tanstack/react-query";
-import { getBookings, getConfigs } from "@/lib/db/queries";
+import { getBookings, getBookingsStats, getConfigs } from "@/lib/db/queries";
 import { Suspense } from "react";
 
 const BookingLayout = async ({ children }: { children: React.ReactNode }) => {
-	const { session } = await getServerSession();
+  const { session } = await getServerSession();
+  const userOrganizationId = session?.session.activeOrganizationId as string;
 
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	await queryClient.prefetchQuery({
-		queryKey: ["bookings", "list"],
-		queryFn: () => getBookings(session?.session.activeOrganizationId as string),
-	});
+  const bookingsStats = await getBookingsStats(userOrganizationId);
 
-	await queryClient.prefetchQuery({
-		queryKey: ["configurations", "package_type"],
-		queryFn: () =>
-			getConfigs(
-				session?.session.activeOrganizationId as string,
-				"package_type",
-			),
-	});
+  await queryClient.prefetchQuery({
+    queryKey: ["bookings", "list"],
+    queryFn: () => getBookings(userOrganizationId),
+  });
 
-	return (
-		<div className="hidden h-full flex-1 flex-col space-y-4 p-8 md:flex relative">
-			<BookingStats />
+  await queryClient.prefetchQuery({
+    queryKey: ["configurations", "package_type"],
+    queryFn: () => getConfigs(userOrganizationId, "package_type"),
+  });
 
-			<Suspense fallback={<div>Loading...</div>}>
-				<HydrationBoundary state={dehydrate(queryClient)}>
-					<div>{children}</div>
-				</HydrationBoundary>
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className="hidden h-full flex-1 flex-col space-y-4 p-8 md:flex relative">
+      <Suspense fallback={<div>Loading...</div>}>
+        <BookingStats initialStats={bookingsStats} />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <div>{children}</div>
+        </HydrationBoundary>
+      </Suspense>
+    </div>
+  );
 };
 
 export default BookingLayout;
