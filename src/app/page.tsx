74dec40959f241282be1c0390758ee198c68a70f
ra-kesh <iamrakeshkumar@pm.me>
2025-07-06 17:46:08 +0530

    added protected layout for dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const wtp = () => {
-	return <div>Wedding tales studio solutions</div>;
+	return (
+		<>
+			<div>Wedding tales studio solutions</div>
+			<Link
+				prefetch={true}
+				href="/home"
+				className="cursor-pointer font-semibold"
+			>
+				Home
+			</Link>
+		</>
+	);
 };
 
 export default wtp;
---
+++ src/app/restricted-to-roles.tsx
@@ -0,0 +1,38 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import type * as React from "react";
+import { auth } from "@/lib/auth";
+
+interface ProtectedProps {
+	children: React.ReactNode;
+	permissions: { [key: string]: string[] };
+	fallback?: React.ReactNode;
+	redirectPath?: string;
+}
+
+export async function Protected({
+	children,
+	permissions,
+	fallback,
+	redirectPath = "/not-found",
+}: ProtectedProps) {
+	const hasAccess = await auth.api.userHasPermission({
+		headers: await headers(),
+		body: {
+			permissions,
+		},
+	});
+
+	if (hasAccess.success) {
+		return <>{children}</>;
+	}
+
+	if (fallback) {
+		return <>{fallback}</>;
+	}
+
+	redirect(redirectPath);
+
+	// This part is unreachable due to the redirect, but keeps TypeScript happy.
+	return null;
+}
